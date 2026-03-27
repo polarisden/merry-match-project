5386471e26288d934857b28dcb441bd250c79d1a
parallell99 <jr.projectx@gmail.com>
2026-03-27 13:29:03 +0700

    feat: implement registration page with multi-step form and update routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import TestPage from '@/views/TestPage.vue'
+import RegisterPage from '@/views/RegisterPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage },
     { path: '/test', component: TestPage },
+    { path: '/Register', component: RegisterPage },
   ],
 })
 
