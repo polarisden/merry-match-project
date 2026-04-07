c92d78d57adfd29b0eb278fc3a53d5e7f7a97313
polaris <dinsor1545@gmail.com>
2026-04-07 14:27:15 +0700

    feat: add ApiHealthPage component for testing API connection

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import TestPage from '@/views/TestPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import MatchingPage from '@/views/MatchingPage.vue'
+import ApiHealthPage from '@/views/ApiHealthPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage },
     { path: '/test', component: TestPage },
+    { path: '/api-health', component: ApiHealthPage },
     { path: '/Register', component: RegisterPage },
     {
       path: '/profile',
